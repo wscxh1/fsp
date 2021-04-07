ccf4bb676fd2b74df81020dfada429bc195a6090
asuta <hxcsw1@foxmail.com>
2021-04-07 16:20:27 +0800

    去除黑色边框

--- src/main/java/com/foodshare/service/impl/VerifyCodeServiceImpl.java
+++ src/main/java/com/foodshare/service/impl/VerifyCodeServiceImpl.java
@@ -88,8 +88,8 @@ public class VerifyCodeServiceImpl implements VerifyCodeService {
         // 设置字体。
         gd.setFont(font);
         // 画边框。
-        gd.setColor(Color.BLACK);
-        gd.drawRect(0, 0, verifyCode.width - 1, verifyCode.height - 1);
+//        gd.setColor(Color.BLACK);
+//        gd.drawRect(0, 0, verifyCode.width - 1, verifyCode.height - 1);
         // 随机产生160条干扰线，使图象中的认证码不易被其它程序探测到。
         gd.setColor(Color.gray);
         for (int i = 0; i < 16; i++) {
